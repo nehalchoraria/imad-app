99fcd4e08d6bd63191b9c349171fee7ff117f662
nehalchoraria <nehalchoraria09@gmail.com>
2017-08-13 01:25:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one' , function(req,res){
+app.get('/article_one' , function(req,res){
     res.send("Aricle One Requested")
 });
 
