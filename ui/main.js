8b82a74dc1ff558d6acdb674e381fbc790a25b31
nehalchoraria <nehalchoraria09@gmail.com>
2017-08-13 19:07:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,15 +4,26 @@ img = document.getElementById('madi');
 
 margin = 0 ;
 
-function Move()
+function MoveRight()
+{
+    margin = margin + 5 ; 
+    img.style.marginRight = margin + 'px' ; 
+}
+
+function MoveLeft()
 {
     margin = margin + 5 ; 
     img.style.marginLeft = margin + 'px' ; 
 }
 
+counter = 0;
+
 img.onclick =  function()
 {
-    setInterval(Move,10) ; 
+    if (counter%2 === 0)
+    setInterval(MoveRight,50) ; 
+    else
+    setInterval(MoveLeft,50) ; 
 };
 
 //img.onclick = alert('Hi') ;
