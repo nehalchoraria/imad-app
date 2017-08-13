a3d109a7a85ed0e6594c78a9e99f6eb8d2b27dbf
nehalchoraria <nehalchoraria09@gmail.com>
2017-08-13 21:58:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,33 +1,59 @@
-console.log('Loaded!');
 
-img = document.getElementById('madi');
+var button = document.getElementById('counter');
 
-margin = 0 ;
-
-function MoveRight()
+button.onclick() = function()
 {
-    margin = margin + 5 ; 
-    img.style.marginRight = margin + 'px' ; 
-}
-
-function MoveLeft()
-{
-    margin = margin - 5 ;
-    img.style.marginRight = margin + 'px' ; 
-}
-
-counter = 0;
-
-img.onclick =  function()
-{
-    counter= counter + 1 ; 
-    console.log(counter)
     
-    
-    if (counter%2==0)
-    setInterval(MoveRight,50) ; 
-    else
-    setInterval(MoveLeft,50) ; 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('Loaded!');
+
+// img = document.getElementById('madi');
+
+// margin = 0 ;
+
+// function MoveRight()
+// {
+//     margin = margin + 5 ; 
+//     img.style.marginRight = margin + 'px' ; 
+// }
+
+// function MoveLeft()
+// {
+//     margin = margin - 5 ;
+//     img.style.marginRight = margin + 'px' ; 
+// }
+
+// counter = 0;
+
+// img.onclick =  function()
+// {
+//     counter= counter + 1 ; 
+//     console.log(counter)
+    
+    
+//     if (counter%2==0)
+//     setInterval(MoveRight,50) ; 
+//     else
+//     setInterval(MoveLeft,50) ; 
+// };
 
 //img.onclick = alert('Hi') ;
