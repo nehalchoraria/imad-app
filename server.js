7b3c6da31bb39c38a5f267364a73b154dd7fe038
nehalchoraria <nehalchoraria09@gmail.com>
2017-08-28 21:56:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/comments' , function(reqt,res)
 
 app.get('/hash/:input',function(req,res)
 {
-    var h = hash(req.params.input,'a')
+    var h = hash(req.params.input,'ab')
     res.send(h)
     
 });
